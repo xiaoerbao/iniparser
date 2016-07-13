e884f8d12a49dcccb5b69308cbe8857acecf2c17
Ma <laomafeima@gmail.com>
2016-07-13 17:09:11 +0800

    添加文档

--- python/iniparse.py
+++ python/iniparse.py
@@ -4,7 +4,7 @@
 import io
 import os
 """
-A INI file parser
+INI 格式文件解析器
 """
 
 version = "0.0.1"
@@ -14,27 +14,38 @@ version_info = (0, 0, 1)
 class INIParse(dict):
 
     def __init__(self):
-        self.comment_symbol = [";"]
-        self.section_symbol = ("[", "]")
-        self.string_symbol = ("\"", "'")
-        self.true_boolean_symbol = ("true",)
-        self.false_boolean_symbol = ("false",)
-        self.list_symbol = "[]"
-        self.include_symbol = "@include "
-        self.delimiters = ("=", ":")
-        self.section_delimiters = "."
+        """
+        返回一个 INIParse 对象，后续的一些操作都是基于这个对象进行的。
+        如果要做修改。可以在实例化INIParse对象之后来修改默认的各种分隔符，这些分隔符在属性里面。
+        """
+        self.comment_symbol = [";"] # 注释识别符，可以支持多个
+        self.section_symbol = ("[", "]") # 分组识别标识，第一项是开头，第二项是结尾
+        self.string_symbol = ("\"", "'") # 字符串识别符号。
+        self.true_boolean_symbol = ("true",) # bool 类型里面 True 的关键字
+        self.false_boolean_symbol = ("false",) # bool 类型里面 False 的关键字
+        self.none_boolean_symbol = ("null", "none") # 表示 None 类型的关键字
+        self.list_symbol = "[]" # key 后面用来表示这个是list 的表示
+        self.include_symbol = "@include " # include 操作识别
+        self.delimiters = ("=", ":") # key-value 分隔符
+        self.section_delimiters = "." # 分组里面表示层级关系的符号
         self.file_list = []
         self.read_file_list = []
         self.read_file_encoding = None
-        self._cursor = {}
+        self._cursor = {} # 游标
 
     def __getattr__(self, name):
+        """
+        支持对象方式获取属性
+        """
         try:
             return self[name]
         except KeyError:
             raise AttributeError(name)
 
     def read(self, filenames, encoding=None):
+        """
+        读取配置文件，可以传入一个数组，支持多个配置文件解析
+        """
         self.read_file_encoding = encoding
         if isinstance(filenames, str):
             filenames = [filenames]
@@ -49,6 +60,9 @@ class INIParse(dict):
         return self.read_file_list
 
     def read_file(self, f, source=None):
+        """
+        读取配置文件，只支持一个，参数是文件句柄
+        """
         if source is None:
             try:
                 source = f.name
@@ -58,14 +72,22 @@ class INIParse(dict):
         self._read(f, source)
 
     def read_string(self, string, source='<string>'):
-        """Read configuration from a given string."""
+        """
+        从字符串中读取配置文件。
+        """
         sfile = io.StringIO(string)
         self._read(sfile, source)
 
     def read_dict(self, dictionary):
+        """
+        从字典里面加载进配置文件里面。可以用于字典格式转换配置文件格式
+        """
         self.update(dictionary)
 
     def _read(self, fp, fpname):
+        """
+        读取和解析每一行内容
+        """
         self.file_list.append(fpname)
         for line in fp:
             line = line.strip()
@@ -83,6 +105,9 @@ class INIParse(dict):
         fp.close()
 
     def _parse_include(self, line, fp):
+        """
+        处理 include 操作，支持相对路径和绝对路径
+        """
         filename = line[len(self.include_symbol):]
         filename = filename.strip().strip("\"")
         if not os.path.isabs(filename):
@@ -96,6 +121,9 @@ class INIParse(dict):
                 return
 
     def _parse_section(self, line):
+        """
+        解析分组，支持无限极的嵌套即可以有很多个 . 表示层级 [test.test.test.......]
+        """
         line = line.strip("[").strip("]") + "."
         self._cursor = self
         while self.section_delimiters in line:
@@ -107,6 +135,9 @@ class INIParse(dict):
             self._cursor = self._cursor[group]
 
     def _parse_expression(self, line):
+        """
+        解析每个表达式，这里会尝试进行类型转换
+        """
         line = line.strip()
         for delimiter in self.delimiters:
             index = line.find(delimiter)
@@ -132,6 +163,8 @@ class INIParse(dict):
                 value = True
             elif value.lower() in self.false_boolean_symbol:
                 value = False
+            elif value.lower() in self.none_boolean_symbol:
+                value = None
             elif value.isnumeric():
                 value = int(value)
             else:
@@ -152,6 +185,9 @@ class INIParse(dict):
             self._cursor[key] = value
 
     def _check_variable(self, variable, return_bool=True):
+        """
+        处理层级嵌套的时候检查是否已经存在
+        """
         return_value = {"variable": None, "section": None}
         temp_cursor = self
         if self.section_delimiters in variable:
@@ -175,6 +211,9 @@ class INIParse(dict):
             return False if return_bool else return_value
 
     def _get_variable(self, variable):
+        """
+        获取已经存在的分组的游标
+        """
         result = self._check_variable(variable, False)
         if result["section"] is None:
             print("Error")
@@ -182,37 +221,58 @@ class INIParse(dict):
             return result["section"][result["variable"]]
 
     def refresh(self):
+        """
+        刷新操作，会重新需从文件里面加载配置项。字符串和字典方式不支持刷新
+        """
         self.clear()
         self.read(self.read_file_list)
 
     def sections(self):
+        """
+        返回当前有多少个子项的key
+        """
         return self.keys()
 
     def get_section(self, section):
+        """
+        获取子项的内容
+        """
         if section in self:
             return self[section]
         else:
             return None
 
     def set_sections(self, section, dictionary):
+        """
+        修改子项的内容
+        """
         if section in self:
             self[section].update(dictionary)
         else:
             self[section] = dictionary
 
     def get_int(self, key):
+        """
+        获取一个值，在这里尝试做int转换
+        """
         if self.has(key):
             return int(self[key])
         else:
             return False
 
     def get_float(self, key):
+        """
+        获取一个值，在这里尝试做float转换
+        """
         if self.has(key):
             return float(self[key])
         else:
             return False
 
     def get_bool(self, key):
+        """
+        获取一个值，在这里尝试做bool转换
+        """
         if self.has(key):
             if str(self[key]).lower() in self.true_boolean_symbol:
                 return True
@@ -224,28 +284,46 @@ class INIParse(dict):
             return False
 
     def get_string(self, key):
+        """
+        获取一个字符串值
+        """
         if self.has(key):
             return str(self[key])
         else:
             return False
 
     def is_none(self, key):
+        """
+        判断是否是None类型
+        """
         if self.has(key) and self[key] is None:
             return True
         else:
             return False
 
     def has(self, key):
+        """
+        检查是否包含子项
+        """
         return True if key in self else False
 
     def write_file(self, f):
+        """
+        写入配置文件，会把自己转换成ini格式写入到文件。参数为文件句柄
+        """
         string = self.stringify()
         return f.write(string)
 
     def stringify(self):
+        """
+        把配置项转换为ini格式的字符串，方便保存到其他地方，如数据库等。
+        """
         return self._section_stringify(self)
 
     def _section_stringify(self, section, prefix=""):
+        """
+        对每个分组进行转换城字符串操作。
+        """
         string = ""
         for (key, val) in section.items():
             if isinstance(val, INIParse):
