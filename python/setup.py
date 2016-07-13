6939d0512d9ece2454484fdf6c1c57d75b88b572
老馬 <laomafeima@gmail.com>
2016-07-13 21:14:02 +0800

    Create setup.py

---
+++ python/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+setup(
+    name="INIParser",
+    version=VERSION,
+    description="A INIParser .",
+    long_description='INI file format parsing just on Python3',
+    keywords='INI parse parser',
+    author='laoma',
+    author_email='laomafeima@gmail.com',
+    url='https://github.com/laomafeima/iniparse',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    packages=find_packages(),
+    )
